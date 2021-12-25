907960f49fcf000ba46bb12bcf09d2319fc44050
Sofia <sofiaq1@gmail.com>
2021-12-25 20:54:59 +0100

    Added StartPage.jsx

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Container from '../components/Container'
+import { Link } from 'react-router-dom'
+import H1 from '../components/H1'
 
 export default function StartPage() {
     return (
-        <div>
-            
-        </div>
+        <Container centered width={400}>
+            <H1>iCRM</H1>
+            <Link to="/login">Klicka här för att gå till inloggningssidan</Link>
+        </Container>
     )
 }
