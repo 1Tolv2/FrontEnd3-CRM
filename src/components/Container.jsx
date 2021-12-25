89cd1e1ac1bad9b1ed170f31172707cf095a2c8c
Sofia <sofiaq1@gmail.com>
2021-12-25 20:53:00 +0100

    new container variation

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,15 +3,19 @@ import styled from 'styled-components'
 
 const StyledContainer = styled.div`
 background-color: #f0f0f0;
-margin: 100px auto;
 width: ${props => props.width}px;
 padding: 5px 5px 5px 5px;
 border: solid grey 1px;
 `
+const CenteredContainer = styled(StyledContainer)`
+margin: 100px auto;
+`
 export default function Container(props) {
     return (
-        <StyledContainer width={props.width}>
+        <>
+        {props.centered ? <CenteredContainer width={props.width}>
             {props.children}
-        </StyledContainer>
+        </CenteredContainer> : <StyledContainer>{props.children}</StyledContainer>}
+        </>
     )
 }
