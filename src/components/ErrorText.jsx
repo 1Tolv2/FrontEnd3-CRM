749d561eadcbcc158d0c9383ac1ecec1faca557d
Sofia <sofiaq1@gmail.com>
2021-12-28 15:39:17 +0100

    Added gridProperties

--- src/components/ErrorText.jsx
+++ src/components/ErrorText.jsx
@@ -4,12 +4,14 @@ import styled from 'styled-components'
 const RedText = styled.p`
 color: red;
 margin: 0 0 10px 0;
+grid-column-start: ${props => props.gridStart};
+grid-column-end: ${props => props.gridEnd};
 `
 
 
 export default function ErrorText(props) {
     return (
-        <RedText>
+        <RedText gridStart={props.gridStart} gridEnd={props.gridEnd}>
             {props.children}
         </RedText>
     )
