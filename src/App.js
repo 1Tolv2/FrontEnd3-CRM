02a02df9b548e7240c88d2a7d3c0c08717885a30
Sofia <sofiaq1@gmail.com>
2021-12-30 22:03:52 +0100

    Added globalStyle and darkmode

--- src/App.js
+++ src/App.js
@@ -7,30 +7,54 @@ import UserCreatePage from "./pages/UserCreatePage";
 import StartPage from "./pages/StartPage";
 import H1 from "./components/H1";
 import ModeSwitch from "./components/ModeSwitch";
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
+*{transition: 0.4s;}
+body {
+margin: 0;
+font-family: "calibri";
+background-color: ${(props) => (props.isDarkMode ? "#212121" : "#ebebeb")}};
+a {
+  text-decoration: none;
+  color: inherit;
+  font-weight: bold;
+}
+.delete {
+  background-color: #ff3300;
+}
+h3 {
+  padding: 0 20px;
+}
+`;
+const DarkThemeContext = createContext([]);
 const CustomerContext = createContext([]);
 const UserContext = createContext([]);
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
   const [customerList, setCustomerList] = useState(null);
   const [user, setUser] = useState(null);
   return (
     <>
-      <H1>iCRM</H1>
-      <ModeSwitch />
-      <UserContext.Provider value={{ user, setUser }}>
-        <CustomerContext.Provider value={{ customerList, setCustomerList }}>
-          <Routes>
-            <Route path="/" element={<StartPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/create-user" element={<UserCreatePage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/customer/:id" element={<CustomerDetailPage />} />
-          </Routes>
-        </CustomerContext.Provider>
-      </UserContext.Provider>
+      <DarkThemeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
+        <GlobalStyle isDarkMode={isDarkMode} />
+        <H1>iCRM</H1>
+        <ModeSwitch />
+        <UserContext.Provider value={{ user, setUser }}>
+          <CustomerContext.Provider value={{ customerList, setCustomerList }}>
+            <Routes>
+              <Route path="/" element={<StartPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/create-user" element={<UserCreatePage />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/customer/:id" element={<CustomerDetailPage />} />
+            </Routes>
+          </CustomerContext.Provider>
+        </UserContext.Provider>
+      </DarkThemeContext.Provider>
     </>
   );
 }
 
 export default App;
-export { CustomerContext, UserContext };
+export { CustomerContext, UserContext, DarkThemeContext };
