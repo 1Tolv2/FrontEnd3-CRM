2753a79429dfbae137b770489a74b15bae389e1c
Sofia <sofiaq1@gmail.com>
2022-01-01 16:24:49 +0100

    Animation added

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,21 +1,23 @@
-import React, {useContext} from 'react'
-import styled, {css} from 'styled-components'
+import React, { useContext } from "react";
+import styled, { css } from "styled-components";
 import { DarkThemeContext } from "../App";
 
 const Grid = styled.div`
   display: grid;
   grid-template-columns: ${(props) => props.gridColTemplate};
   padding: 10px;
+  transition: 0.4s;
+
   ${(props) =>
     props.gap &&
     css`
       gap: 10px;
     `}
-    @media (max-width: 1000px) {
-      display: flex;
-      flex-flow: column wrap;
-      padding: 0;
-      }
+  @media (max-width: 1000px) {
+    display: flex;
+    flex-flow: column wrap;
+    padding: 0;
+  }
 `;
 
 const Item = styled.div`
@@ -24,19 +26,40 @@ const Item = styled.div`
   grid-row-start: ${(props) => props.rowStart};
   grid-row-end: ${(props) => props.rowEnd};
   width: ${(props) => props.width}px;
-  padding: 5px 10px 5px 10px;
-  background-color: ${props => props.isDarkMode ? "#333333" : "white"};
+  padding: 5px 20px 5px 20px;
+  background-color: ${(props) => (props.isDarkMode ? "#333333" : "white")};
+
+  ${(props) =>
+    props.transitionIn &&
+    css`
+      animation: display 0.6s ease-out;
+
+      @keyframes display{
+        0% {
+          transform: translateY(-100%) scale(0.9);
+          z-index: -1;
+      }
+      100% {
+          transform: translateY(0) scale(1);
+          z-index: -1;
+      }
+      }
+    `}
 `;
 
 export default function GridContainer(props) {
-  const {isDarkMode} = useContext(DarkThemeContext)
+  const { isDarkMode } = useContext(DarkThemeContext);
 
   return (
     <>
       {props.item ? (
-        <Item {...props} isDarkMode={isDarkMode}>{props.children}</Item>
+        <Item {...props} isDarkMode={isDarkMode}>
+          {props.children}
+        </Item>
       ) : (
-        <Grid {...props} isDarkMode={isDarkMode}>{props.children}</Grid>
+        <Grid {...props} isDarkMode={isDarkMode}>
+          {props.children}
+        </Grid>
       )}
     </>
   );
