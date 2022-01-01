c66d04b05ccc6a9052d76fa72258984be0900d8e
Sofia <sofiaq1@gmail.com>
2022-01-02 00:30:10 +0100

    Added Tokens.js

---
+++ src/components/Tokens.js
@@ -0,0 +1,31 @@
+const tokens = {
+  lightTheme: {
+    darkGrey1: "#757575",
+    darkGrey2: "grey",
+    grey: "#ccc",
+    white: "white",
+    blue: "#303ecf",
+    lightBlue: "#33cccc",
+    lightGreen: "#00e66f",
+    whiteGreen: "#ebf55ee",
+    red: "#ff3300",
+    paleRed: "#ff5050",
+  },
+
+  darkTheme: {
+    black: "black",
+    darkGrey1: "#212121",
+    darkGrey3: "#424242",
+    darkGrey2: "#333333",
+    darkGrey4: "#757575",
+    purpleBlue: "#6e78dd",
+    blueGrey: "#415358",
+    lightBlue: "#79E2F2",
+    lightBlue2: "#80ffff",
+    lightGreen: "#44d598",
+    red: "#ff3300",
+    paleRed: "#e25050",
+  },
+};
+
+export default tokens;
