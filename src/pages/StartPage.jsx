2f03b7e8ff85a4e7f376ab874d868b1aceeb39a0
Sofia <sofiaq1@gmail.com>
2022-01-17 00:44:19 +0100

    Minor style changes

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import CenteredContainer from "../components/CenteredContainer";
+import FlexBox from "../components/FlexBox";
 import H1 from "../components/H1";
 import ButtonLink from "../components/ButtonLink";
-import Grid from "../components/Grid";
 import Background from "../components/Background";
 
 export default function StartPage() {
@@ -12,18 +12,14 @@ export default function StartPage() {
     <Background>
       <CenteredContainer>
         <H1>WorkSpace</H1>
-        <Grid center gridColTemplate="auto auto">
-          <Grid item>
+        <FlexBox>
             <Link to="/login">
               <ButtonLink>SIGN IN</ButtonLink>
             </Link>
-          </Grid>
-          <Grid item>
             <Link to="/create-user">
               <ButtonLink>REGISTER</ButtonLink>
             </Link>
-          </Grid>
-        </Grid>
+        </FlexBox>
       </CenteredContainer>
     </Background>
   );
